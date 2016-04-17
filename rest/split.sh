63323e5387682ff7ec240b4818b774e53c701ec4
Michela Paganini <micky.91@hotmail.com>
2016-04-17 20:20:19 +0200

    adding split.sh

---
+++ rest/split.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PATH_TO_VID="$1"
+PIC_FOLDER="$2"
+PATH_TO_AUDIO="$3"
+
+echo "$PATH_TO_VID"
+echo "$PIC_FOLDER"
+echo "$AUDIO_FOLDER"
+
+# if [ ! -d "$AUDIO_FOLDER" ]; then
+#   mkdir "$AUDIO_FOLDER"
+# fi
+
+ffmpeg -i "$PATH_TO_VID" -ab 160k -ac 2 -ar 44100 -vn "$PATH_TO_AUDIO" -r 10 -y small.avi
+
+if [ ! -d "$PIC_FOLDER" ]; then
+  mkdir "$PIC_FOLDER"
+fi
+
+ffmpeg -i small.avi -y "$PIC_FOLDER"/pic%d.jpg
+
+rm small.avi
